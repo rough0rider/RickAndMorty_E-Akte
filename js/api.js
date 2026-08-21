b936f8e47fb54d0c3c63293088a50ca7e7615c99
rough0rider <samuel.khodak@gmail.com>
2026-08-21 09:46:57 +0200

    Refactor API functions for improved clarity and consistency; enhance comments for better understanding

--- js/api.js
+++ js/api.js
@@ -5,27 +5,22 @@ const API_URL = "https://rickandmortyapi.com/api";
 // Charaktere laden
 // ========================================
 
+// Holt eine Seite Charaktere von der API (20 pro Seite). page = 1, falls kein
+// Wert übergeben wird
 function getCharacters(page = 1) {
 
-    // $.ajax() liefert ein jqXHR-Objekt zurück,
-    // das sich wie ein Promise verhält und
-    // daher ganz normal mit await abgewartet
-    // werden kann.
-
+    //$.ajax() schickt die Anfrage im Hintergrund (kein Neuladen der Seite)
     return $.ajax({
-
         url: `${API_URL}/character`,
-
+        // "GET" = nur Daten abholen
         method: "GET",
-
+        // {page} wird automatisch zu "?page=2" in der URL
         data: {
             page: page
         },
-
+        // "json" wandelt die Antwort automatisch in ein JS-Objekt um
         dataType: "json"
-
     });
-
 }
 
 
@@ -35,49 +30,32 @@ function getCharacters(page = 1) {
 
 function searchCharacters(name) {
 
-    // $.getJSON() ist die Kurzschreibweise für
-    // $.ajax mit method: "GET" und
-    // dataType: "json"
-
     const deferred = $.Deferred();
 
-
+    // Kurzform von $.ajax() für GET-Anfragen mit JSON-Antwort
     $.getJSON(
-
         `${API_URL}/character/`,
-
         {
+            // {name: name} wird automatisch zu "?name=..." in der URL
             name: name
         }
-
     )
-
+    // "done" und "fail" sind die beiden möglichen Ergebnisse der Anfrage
+    // und abhängig vom Ergebnis wird entweder die "resolve"- oder 
+    // "reject"-Funktion (404) des Deferred-Objekts aufgerufen
         .done(data => {
-
             deferred.resolve(data.results);
-
         })
-
         .fail(jqXHR => {
-
-            // Bei keiner Übereinstimmung
-            // liefert die API 404
-
+            // Bei keiner Übereinstimmung liefert die API Error 404
             if (jqXHR.status === 404) {
-
                 deferred.resolve([]);
-
             } else {
-
                 deferred.reject(jqXHR);
-
             }
-
         });
 
-
     return deferred.promise();
-
 }
 
 
@@ -87,14 +65,12 @@ function searchCharacters(name) {
 
 function getCharacter(id) {
 
+    //$.ajax() schickt die Anfrage im Hintergrund (kein Neuladen der Seite)
     return $.ajax({
-
         url: `${API_URL}/character/${id}`,
-
+        // "GET" = nur Daten abholen
         method: "GET",
-
+        // "json" wandelt die Antwort automatisch in ein JS-Objekt um
         dataType: "json"
-
     });
-
 }
